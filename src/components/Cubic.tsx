04add8b9ac8767bf7a5b7283bdc2332e89310727
Kirill <Prokhorov187@yandex.ru>
2025-06-23 13:14:43 +0300

    add input for color of number

--- src/components/Cubic.tsx
+++ src/components/Cubic.tsx
@@ -14,6 +14,7 @@ const Cubic = ({d, imgSrc, isAnimated}: CubicProps) => {
     const [count, setCount] = useState<number>();
     const [isDisable, setIsDisable] = useState<boolean>();
     const [bgColor, setBgColor] = useState<string>('');
+    const [numColor, setNumColor] = useState('');
 
     useEffect(() => {
 
@@ -42,9 +43,15 @@ const Cubic = ({d, imgSrc, isAnimated}: CubicProps) => {
 
     
 
-    function handleChangeColor(e: React.ChangeEvent<HTMLInputElement>){
+    function handleChangeBgColor(e: React.ChangeEvent<HTMLInputElement>){
         setBgColor(e.target.value)
     }
+
+    function handleChangeNumColor(e: React.ChangeEvent<HTMLInputElement>){
+        setNumColor(e.target.value)
+    }
+
+
 
 return (
     <>
@@ -55,13 +62,14 @@ return (
             <h2>Кубик d{d}</h2>
 
             <div className="wrapper animate__animated" id={`wrap${d}`} style={{backgroundImage: `url('src/images/${imgSrc}.png')`}}>
-                <span className={count === d || count === 1 ? 'redCount' : 'count'}>{count}</span>
+                <span style={{color: numColor}} className={count === d || count === 1 ? 'redCount' : 'count'}>{count}</span>
             </div>
 
             {!isDisable ? <button onClick={getCubicNumber} className="btn-getNum" disabled={isDisable}>Бросить кубик</button> : <div style={{height: '40px'}}></div>}
 
             <form action="#" className="get_animation">
-                <label htmlFor="bgColor" className="inputColor"><input type="color" name="bgColor" id="bgColor" onChange={handleChangeColor}/>Цвет фона</label>
+                <label htmlFor="bgColor" className="inputColor"><input type="color" name="bgColor" id="bgColor" onChange={handleChangeBgColor}/>Цвет фона</label>
+                <label htmlFor="numColor" className="inputColor"><input type="color" name="numColor" id="numColor" onChange={handleChangeNumColor}/>Цвет цифры</label>
             </form>
 
         </div> 
